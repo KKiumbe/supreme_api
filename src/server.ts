94219cb7783e4728f2cf1f6cd9711d6e2447073a
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2025-11-01 17:00:06 +0300

    update

--- src/server.ts
+++ src/server.ts
@@ -7,27 +7,24 @@ import authRoutes from './routes/userRoute';
 import metersRoutes from './routes/meterInventoryRoute/inventory';
 import tarrifsRoutes from './routes/tarrifsRoute/tarrifs';
 import schemesZoneRoutes from './routes/schemeZoneRoute/location';
-import customersRoutes from './routes/customersRoute/customers'
-import meterReadingRoutes from './routes/meterReadingRoutes/readings'
-import connectionRoutes from './routes/connectionsRoute/connections'
+import customersRoutes from './routes/customersRoute/customers';
+import meterReadingRoutes from './routes/meterReadingRoutes/readings';
+import connectionRoutes from './routes/connectionsRoute/connections';
+import billsRoutes from './routes/billsRoute/bills';
+import billTypesRoutes from './routes/billtypesRoutes/billTypes';
 
-import billsRoutes from './routes/billsRoute/bills'
-
-import billTypesRoutes from './routes/billtypesRoutes/billTypes'
 dotenv.config();
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
-
-
 const allowedOrigins = ['https://frolicking-speculoos-9c49e3.netlify.app'];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
+      if (!origin) return callback(null, true); // allow Postman or server-to-server requests
       if (!allowedOrigins.includes(origin)) {
         return callback(new Error('CORS Not Allowed'), false);
       }
@@ -37,30 +34,16 @@ app.use(
   })
 );
 
-// Preflight requests for all routes
-app.options('/*', cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
-
-
-
-
+// Routes
 app.use('/api', authRoutes);
 app.use('/api', metersRoutes);
 app.use('/api', tarrifsRoutes);
-
 app.use('/api', customersRoutes);
-
 app.use('/api', schemesZoneRoutes);
-
-
 app.use('/api', connectionRoutes);
 app.use('/api', meterReadingRoutes);
-
-app.use('/api', billsRoutes)
-
-app.use('/api', billTypesRoutes)
+app.use('/api', billsRoutes);
+app.use('/api', billTypesRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running 🚀');
